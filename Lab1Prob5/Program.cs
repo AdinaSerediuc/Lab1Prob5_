03ef80f930208d099af80f7487575fbc49a48912
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-09-19 19:57:16 +0300

    Prob5

--- Lab1Prob5/Program.cs
+++ Lab1Prob5/Program.cs
@@ -8,6 +8,17 @@ namespace Lab1Prob5
     {
         static void Main(string[] args)
         {
+            /*Ex 5
+Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cele doua valori in ordine crescatoare.
+• Exemplu: citim ,9,0 Afisam : 0 9 
+           */
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x < y)
+                Console.Write(x + "," + y );
+            else
+                Console.WriteLine(y + "," + x );
         }
     }
 }
